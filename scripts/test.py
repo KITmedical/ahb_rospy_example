71562249bfb904a686056e89beb666438378f40f
virtubuntu 14.04 <andreas.bihlmaier@gmx.de>
2014-07-03 09:29:59 +0200

    rospy template

---
+++ scripts/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import ahb_rospy_example.hello
+
+
+def main():
+  ahb_rospy_example.hello.greet()
+
+
+
+if __name__ == '__main__':
+  main()
